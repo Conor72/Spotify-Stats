3c5f1e43a389ca286623c8301b82dbf1a2e5dfe7
Conor Brett <conorbrett72@gmail.com>
2022-08-25 21:58:11 -0400

    Displays current playing song on spotify

--- spotify-python-project.py
+++ spotify-python-project.py
@@ -1,6 +1,13 @@
 import config
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import requests
+import time
+
+from pprint import pprint
+
+SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
+access_token = config.access_token
 
 cid = config.ClientID
 secret = config.ClientSecret
@@ -10,10 +17,60 @@ sp = spotipy.Spotify(client_credentials_manager
 client_credentials_manager)
 
 
-urn = 'spotify:artist:70cRZdQywnSFp9pnc2WTCE'
+#ID for Simon & Garfunkel - https://open.spotify.com/artist/70cRZdQywnSFp9pnc2WTCE?si=vLHMSEoAR-WXVDT4O1a9Sw&nd=1
+#urn = 'spotify:artist:70cRZdQywnSFp9pnc2WTCE'
 
-artist = sp.artist(urn)
-print(artist)
+#artist = sp.artist(urn)
+#print(artist)
 
-user = sp.user('plamere')
-print(user)
+#user = sp.user('plamere')
+#print(user)
+
+
+
+def get_current_track(access_token):
+    print("test")
+    response = requests.get(
+        SPOTIFY_GET_CURRENT_TRACK_URL,
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+    json_resp = response.json()
+    track_id = json_resp['item']['id']
+    track_name = json_resp['item']['name']
+    artists = [artist for artist in json_resp['item']['artists']]
+    print("test2")
+    link = json_resp['item']['external_urls']['spotify']
+
+    artist_names = ', '.join([artist['name'] for artist in artists])
+
+    current_track_info = {
+    	"id": track_id,
+    	"track_name": track_name,
+    	"artists": artist_names,
+    	"link": link
+    }
+    print("test3")
+    pprint(
+		    	current_track_info,
+		    	indent=4,
+		    )
+    return current_track_info
+
+
+get_current_track(access_token)
+
+                             
+
+
+
+#To-Do
+
+#Stats from Artits
+
+#Get Profile picture URL
+
+#Start displaying on webpage
+
+#Display data in bar charts and pie charts
